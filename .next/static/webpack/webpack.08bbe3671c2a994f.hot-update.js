d2c3433075dc58ad8b5448cafcaecb54ca771a22
Marco Haber <marcoshaber99@gmail.com>
2023-09-07 23:09:03 +0300

    progress

--- .next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
+++ .next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
@@ -84,7 +84,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "75eace03e9b1c235"; }
+/******/ 	__webpack_require__.h = function() { return "d2f7961ea965c6a7"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
--- app/api/tickets/route.js
+++ app/api/tickets/route.js
@@ -1,3 +1,15 @@
+import { NextResponse } from "next/server"
 
 
-// api/tickets
+
+export async function GET() { 
+  const res = await fetch("http://localhost:4000/tickets")
+
+  const tickets = await res.json()
+
+  return NextResponse.json(tickets, { 
+    status: 200
+  })
+
+
+}
